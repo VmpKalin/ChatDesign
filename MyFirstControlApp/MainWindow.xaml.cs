86e8211f8501d06f700046488e01a074ebecc32d
VmpKalin <artik-17@mail.ru>
2018-07-09 01:39:10 +0300

    Add project files.

---
+++ MyFirstControlApp/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFirstControlApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPopupExit_OnClick(object sender, RoutedEventArgs e)
+        {
+            //var result = MessageBox.Show("Are u sure?", "Exit", MessageBoxButton.YesNo);
+
+            //if (result == MessageBoxResult.Yes)
+                Application.Current.Shutdown();
+            
+            //e.Handled = true;
+        }
+
+        private void ButtonCloseMenu_OnClick(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            GridMenu.Visibility = Visibility.Collapsed;
+            GridChat.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonOpenMenu_OnClick(object sender, RoutedEventArgs e)
+        {
+            GridMenu.Visibility = Visibility.Visible;
+            GridChat.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+    }
+}
